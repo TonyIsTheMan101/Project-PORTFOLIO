449764e4d0d628d4c3cd7e2868c07ab654e8bbe4
TonyIsTheMan101 <133550536+TonyIsTheMan101@users.noreply.github.com>
2023-05-13 22:53:33 -0700

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,21 +1,31 @@
 <?php
+if(isset($_POST['submit'])) {
+    // Get the form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-// Get the form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+    // Set the recipient email address
+    $to = 'youremail@example.com';
 
-// Send an email with the form data
-$to = 'youremail@example.com';
-$subject = 'New message from your website';
-$body = "Name: $name\n\nEmail: $email\n\nSubject: $subject\n\nMessage: $message";
-$headers = "From: $email";
+    // Set the email subject
+    $email_subject = 'New message from your website';
 
-if (mail($to, $subject, $body, $headers)) {
-  echo 'Message sent successfully!';
-} else {
-  echo 'An error occurred. Please try again later.';
+    // Set the email body
+    $email_body = "Name: $name\nEmail: $email\nSubject: $subject\nMessage: $message";
+
+    // Set the email headers
+    $headers = "From: $email\r\nReply-To: $email\r\n";
+
+    // Send the email
+    if(mail($to, $email_subject, $email_body, $headers)) {
+        // If the email was sent successfully, redirect to a thank you page
+        header('Location: thank_you.html');
+        exit;
+    } else {
+        // If there was an error sending the email, display an error message
+        echo 'An error occurred. Please try again later.';
+    }
 }
-
 ?>
